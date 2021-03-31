50a801db5d8d50f6b742f2a37e33dad2164dc68f
Jay MOULIN <jaymoulin@gmail.com>
2021-03-31 06:39:11 +0200

    Removes cursedware code

--- cursedware/tools.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    sleep: function (milliseconds) {
-        return new Promise(res => {
-            setTimeout(res, milliseconds)
-        });
-    },
-    FREE_RANDOM_ALBUM: '655c3a9a-ef36-4203-bc29-2833cbc3759d'
-}
